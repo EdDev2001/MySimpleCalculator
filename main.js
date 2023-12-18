0dbb22dd5225c52613c8d8cd792a91e35dd0ea5f
EdDev <133908849+EdDev2001@users.noreply.github.com>
2023-12-18 16:59:46 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+let outputscreen=document.getElementById('output-onscreen');
+function display(num){outputscreen.value +=num}
+function calculate(){try{outputscreen.value = eval(outputscreen.value) }catch(err){ alert("invalid") }}
+function clr(){ outputscreen.value=''}
+function del(){outputscreen.value=outputscreen.value.slice(0,-1)}
